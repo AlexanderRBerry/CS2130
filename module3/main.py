5a2f0ac802aab440d48a9a2d051029880642a6d8
Alex Berry <Alexanderberry@mail.weber.edu>
2023-08-28 15:01:47 -0600

    Completed main part 1 Sets
    Completed several print statements in main to demonstrate the functions within sets.py

--- module3/main.py
+++ module3/main.py
@@ -5,20 +5,35 @@ import sets
 def main():
     # Part 1 Sets
     print("Part 1 Sets")
-    A = [1, 4, 3, 6, 7, 7, 7, 7]
-
+    union = "∪"
+    intersection = "∩"
+    A = [1, 4, 3, 6, 7]
     B = [2, 3, 4, 7, 9]
+    C = [1, 2, 4, 5, 6, 7, 8]
 
-    C = [1, 2, 4, 5, 6, 7, 8, 7, 7, 7, 7]
+    print("Lists that represent sets A, B, and C")
+    print("Set A = [1, 4, 3, 6, 7]")
+    print("Set B = [2, 3, 4, 7, 9]")
+    print("Set C = [1, 2, 4, 5, 6, 7, 8]")
+    print("\nSet Operations")
+    print("")
+    print("A {} B".format(union))
+    print(sets.union_of_sets(A, B))
+    print("A {} B".format(intersection))
+    print(sets.intersection_of_sets(A, B))
+    print("A {} B {} C".format(union, union))
+    print(sets.union_of_sets(sets.union_of_sets(A, B), C))
+    print("A {} B {} C".format(intersection, intersection))
+    print(sets.intersection_of_sets(sets.intersection_of_sets(A, B), C))
+    print("(A {} B) {} C".format(union, intersection))
+    print(sets.intersection_of_sets(sets.union_of_sets(A, B), C))
+    print("A {} (B {} C)".format(union, intersection))
+    print(sets.union_of_sets(A, sets.intersection_of_sets(B, C)))
+    print("(A {} C) {} B".format(intersection, union))
+    print(sets.union_of_sets(sets.intersection_of_sets(A, C), B))
+    print("A {} (C {} B)".format(intersection, union))
+    print(sets.intersection_of_sets(A, sets.union_of_sets(C, B)))
 
-    D = sets.intersection_of_sets(A, C)
-    E = sets.union_of_sets(A, B)
-    print(D)
-    print(E)
-    # print("Lists that represent sets A, B, and C")
-    #
-    # print("\nSet Operations")
-    #
     # # Part 2 Functions
     # print("\nPart 2 Functions")
     #
